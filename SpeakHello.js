cbb489621487d89f281ea9ecc12be162ba711bad
tejenderdubey <65384493+tejenderdubey@users.noreply.github.com>
2020-06-27 10:44:44 +0530

    Add files via upload

---
+++ SpeakHello.js
@@ -0,0 +1,17 @@
+(function(window) {
+
+	var speakWord = "Hello";
+
+	var helloSpeaker = function (name) {
+
+		console.log(speakWord + " " + name);
+
+	}
+
+
+
+	window.helloSpeaker = helloSpeaker;
+
+
+
+})(window);
